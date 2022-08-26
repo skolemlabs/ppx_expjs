5358ababe32aaa403ce90f28c6c496b2bbeb095a
Zach Baylin <zach@skolem.tech>
2022-08-26 17:05:54 -0400

    Js_test.func: add tests for optional default arguments

--- js_test/func.ml
+++ js_test/func.ml
@@ -1,2 +1,6 @@
 let add (x : int) (y : int) : int = x + y [@@expjs]
 let prepend ~(prefix : string) (str : string) : string = prefix ^ str [@@expjs]
+
+let substr (str : string) ?(i : int = String.length str) () : string =
+  String.sub str 0 i
+  [@@expjs]
--- js_test/func.test.js
+++ js_test/func.test.js
@@ -10,9 +10,19 @@ test('func.prepend({prefix: "Sk"}, "olem") is "Skolem"', () => {
   expect(res).toBe("Skolem")
 });
 
-test('func.prepend({}, "olem") is throws required argument error', () => {
+test('func.prepend({}, "olem") throws required argument error', () => {
   expect(() => {
     func.prepend({}, "olem")
   }).toThrow(Array) /* OCaml exceptions look like arrays
                       We might want to change this */
+});
+
+test('func.substr({}, "Skolem") is "Skolem"', () => {
+  const res = func.substr({}, "Skolem")
+  expect(res).toBe("Skolem")
+});
+
+test('func.substr({i: 2}, "Skolem") is "Sk"', () => {
+  const res = func.substr({i: 2}, "Skolem")
+  expect(res).toBe("Sk")
 });
