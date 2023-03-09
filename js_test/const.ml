b32b56998b96dc3c74afd15831443f0c40fe65c9
Zach Baylin <zach.baylin@skolem.com>
2023-03-09 14:41:43 -0500

    Test: fix const test

--- js_test/const.ml
+++ js_test/const.ml
@@ -4,7 +4,7 @@ let none_four : int option = None [@@expjs]
 let some_four : int option = Some 4 [@@expjs]
 
 (** Disable the unused variable warning *)
-let dont_export : int = 1 [@@expjs { export = false }] [@@warning "-32"]
+let _dont_export : int = 1 [@@expjs { export = false; name = "dont_export" }]
 
 module Caml_module = struct
   (** This name is used to make sure the other my_str isn't shadowed *)
